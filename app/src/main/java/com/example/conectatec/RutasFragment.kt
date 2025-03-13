970de1f6d8a385cbc7a154e6041a6d78f2cc0c6f
Starenka Susana Ortiz Gallegos <starenka.ortiz@gmail.com>
2025-03-12 21:28:19 -0600

    Agregue las ubicaciones del mapa

--- app/src/main/java/com/example/conectatec/RutasFragment.kt
+++ app/src/main/java/com/example/conectatec/RutasFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
+
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -24,7 +27,7 @@ class RutasFragment : Fragment(), OnMapReadyCallback {
         return inflater.inflate(R.layout.activity_mapa, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val mapFragment = childFragmentManager
@@ -46,6 +49,48 @@ class RutasFragment : Fragment(), OnMapReadyCallback {
         val location = LatLng(19.2534, -99.5721)
         mMap.addMarker(MarkerOptions().position(location).title("¡Aquí nos encontramos!"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }*/
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.mapa) as? SupportMapFragment
+
+        if (mapFragment == null) {
+            val newMapFragment = SupportMapFragment.newInstance()
+            childFragmentManager.beginTransaction()
+                .replace(R.id.mapa, newMapFragment)
+                .commit()
+            newMapFragment.getMapAsync(this)
+        } else {
+            mapFragment.getMapAsync(this)
+        }
+        // Obtener el RadioGroup
+        val grupo: RadioGroup = view.findViewById(R.id.Grupo)
+
+
+
+        // Manejar los cambios de selección
+        grupo.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.zinancantepec -> cambiarUbicacion(LatLng(19.2802083, -99.694791666667), " Estación: Zinancantepec")
+                R.id.toluca -> cambiarUbicacion(LatLng(19.270283333333, -99.641791666667), "Estación: Toluca Centro")
+                R.id.metepec-> cambiarUbicacion(LatLng(19.2775611, -99.573475), "Estación: Metepec")
+                R.id.lerma-> cambiarUbicacion(LatLng(19.2784889, -99.514975), "Estación: Lerma")
+                R.id.santaFe-> cambiarUbicacion(LatLng( 19.364475, -99.2682), "Estación: Santa Fe")
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        cambiarUbicacion(LatLng( 19.2802083, -99.694791666667), "Estación: Zinancantepec") // Ubicación por defecto
+    }
+
+    private fun cambiarUbicacion(ubicacion: LatLng, titulo: String) {
+        mMap.clear() // Limpia marcadores anteriores
+        mMap.addMarker(MarkerOptions().position(ubicacion).title(titulo))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, 15f))
     }
 }
 
